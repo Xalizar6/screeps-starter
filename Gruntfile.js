8bb833303ee5715be6d81ca786224fae450a8f83
Xalizar6 <dave.rukamp@gmail.com>
2020-11-18 00:43:48 -0600

    use Secure Credentials and CLI Flags override

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,50 @@
-let matchdep = require('matchdep');
-let mergeFiles = require('./grunt-scripts/mergeFiles');
+let matchdep = require( 'matchdep' );
+let mergeFiles = require( './grunt-scripts/mergeFiles' );
 
-module.exports = function (grunt) {
-	matchdep.filterAll(['grunt-*', '!grunt-cli']).forEach(grunt.loadNpmTasks);
-	mergeFiles(grunt);
+module.exports = function ( grunt ) {
 
-	grunt.initConfig({
-		screeps: {
-			options: {
-				email:    '*****************',
-				password: '*****************',
-				branch:   'default',
-				ptr:      false,
-			},
-			dist:    {
-				src: ['dist/*.js']
-			}
-		},
+    // Pull defaults (including username and password) from .screeps.json
+    const config = require( './.screeps.json' )
 
-		copy:      {
-			main: {
-				expand:  true,
-				flatten: true,
-				filter:  'isFile',
-				cwd:     'dist/',
-				src:     '**',
-				dest:    'Update This Path'
-			},
-		},
-	});
+    // Allow grunt options to override default configuration
+    const email = grunt.option( 'email' ) || config.email;
+    const password = grunt.option( 'password' ) || config.password;
+    const branch = grunt.option( 'branch' ) || config.branch;
+    const ptr = grunt.option( 'ptr' ) ? true : config.ptr
 
-	grunt.registerTask('main', ['test', 'merge', 'write']);
-	grunt.registerTask('sandbox', ['merge', 'write-private']);
-	grunt.registerTask('merge', 'mergeFiles');
-	grunt.registerTask('write', 'screeps');
-	grunt.registerTask('write-private', 'copy');
+    // Load needed tasks
+    matchdep.filterAll( [ 'grunt-*', '!grunt-cli' ] ).forEach( grunt.loadNpmTasks );
+
+    mergeFiles( grunt );
+
+    grunt.initConfig( {
+        screeps: {
+            options: {
+                email: email,
+                password: password,
+                branch: branch,
+                ptr: ptr,
+            },
+            dist: {
+                src: [ 'dist/*.js' ]
+            }
+        },
+
+        copy: {
+            main: {
+                expand: true,
+                flatten: true,
+                filter: 'isFile',
+                cwd: 'dist/',
+                src: '**',
+                dest: 'Update This Path'
+            },
+        },
+    } );
+
+    grunt.registerTask( 'main', [ 'test', 'merge', 'write' ] );
+    grunt.registerTask( 'sandbox', [ 'merge', 'write-private' ] );
+    grunt.registerTask( 'merge', 'mergeFiles' );
+    grunt.registerTask( 'write', 'screeps' );
+    grunt.registerTask( 'write-private', 'copy' );
 };
